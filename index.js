858038e37303d7a9aeb2cc0a0f11a39146e05a3e
Yuntong Man <yuntongman17@gmail.com>
2018-04-08 03:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians,instruments){
   var bandLength = musicians.length;
   var roles = "";
   for (i = 0; i<=bandLength;i++){
-    roles = musicians[i] + "plays" + instruments[i];
+    roles += musicians[i] + "plays" + instruments[i];
   }
 }
